a38dfc95784e8c4e3f625ea2fa5a7215d0151553
박도륜 <88328436+ryxxn@users.noreply.github.com>
2024-08-11 22:15:01 +0900

    fix: move file style.module.css

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./style.module.css";
+import styles from "../style.module.css";
 import { AlertModalProps } from "../types";
 import { bindClassNames } from "../utils";
 
--- src/components/Confirm.tsx
+++ src/components/Confirm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./style.module.css";
+import styles from "../style.module.css";
 import { ConfirmModalProps } from "../types";
 import { bindClassNames } from "../utils";
 
